bd861c7a09a98537c7e0343ca6a27ab1bdb70147
Joshua Cleland <proxikal@users.noreply.github.com>
2016-02-22 00:48:38 -0500

    Added #shorturl command using bit.ly

---
+++ commands/shorturl.php
@@ -0,0 +1,41 @@
+<?php
+$ini = new INI('config.ini');
+$d = $ini->read('config.ini');
+
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+); 
+
+if($d['apis']['bitly'] != "")
+{
+$msg=str_replace("#shorturl ", "", $message->content);
+  $json_string = file_get_contents("https://api-ssl.bitly.com/v3/shorten?access_token=".$d['apis']['bitly']."&login=USERNAME:PASSWORD&longUrl=".$msg, false, stream_context_create($arrContextOptions));
+  $json = json_decode($json_string);
+  $error = $json->{'status_txt'};
+  if($json->{'status_code'} == "200")
+  {
+  $newurl=$json->{'data'}->{'url'};
+  
+  $message->reply("**Bitly URL Created:** " . $newurl); 
+  }
+  else
+  {
+  // send message that it failed
+  $dar="";
+  if($error = "INVALID_LOGIN")
+  {
+  $dar="Make sure you're adding `&login=username:password` parameter in **commands/shorturl.php** *on line: 15*"; 
+  }
+  $message->reply("**Error Status:** `" . $error . "`" . $dar); 
+  }
+  
+  }
+  else
+  {
+  $message->reply(":red_circle: **Error! ** \n ```You need to setup a bit.ly OAUTH key @ http://bit.ly \n open config.ini in the Paradox directory \n place OAUTH key under [apis] bitly = YORUOAUTH```"); 
+  }
+  
+  ?>
