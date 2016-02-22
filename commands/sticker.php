6f7b476085e7b0f90cab1f9db9b34dbf6d4adc0a
Joshua Cleland <proxikal@users.noreply.github.com>
2016-02-21 21:46:45 -0500

    Added #giphy And #sticker commands

---
+++ commands/sticker.php
@@ -0,0 +1,13 @@
+<?php
+$msg=str_replace("#sticker ", "", $message->content);
+$msg=str_replace(" ", "+", $msg);
+
+ $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://api.giphy.com/v1/stickers/random?api_key=dc6zaTOxFJmzC&tag=' . $msg);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $data = curl_exec($ch);
+	$json = json_decode($data);
+	
+	// $message->channel->sendMessage($json->data->images->fixed_height->url); 
+	$message->channel->sendMessage($json->data->image_original_url);
+	?>
