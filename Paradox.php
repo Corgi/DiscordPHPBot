3b5b3b0603dc7876fc2800e8111c1ce224b8bcb0
Joshua Cleland <proxikal@users.noreply.github.com>
2016-02-23 15:51:23 -0500

    Pushing to version 0.1.1b

--- Paradox.php
+++ Paradox.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+ini_set("log_errors", 1);
+ini_set("error_log", "php-error.log");
+
+
 date_default_timezone_set('US/Eastern');
 error_reporting( error_reporting() & ~E_NOTICE );
 
@@ -59,19 +63,24 @@ $ws = new WebSocket($discord);
 $ws->on('ready', function ($discord) use ($ws) {
 $ini = new INI('config.ini');
 $d = $ini->read('config.ini');
+$pref=$d['settings']['Prefix'];
+
 	$discord->updatePresence($ws, $d['settings']['status'], false);
 
 echo "            #    Login complete, Listening...    #".PHP_EOL;
 echo "            ######################################" . PHP_EOL;
 
-
+// echo "Prefix: " . $d['settings']['Prefix'];
 
 	$ws->on(Event::MESSAGE_CREATE, function ($message, $discord, $newdiscord) use ($ws) {
 
-	
+
+	$ini = new INI('config.ini');
+$d = $ini->read('config.ini');
+$pref=$d['settings']['Prefix'];
 
 
-$dat = explode("#", $message->content); // using normal param #paradox example #paradox who am i?
+$dat = explode($d['settings']['Prefix'], $message->content); // using normal param #paradox example #paradox who am i?
 $rawdat=$dat[1];
 
 $special = explode("<@147463276840747008> ", $message->content); // using special param @Paradox instead of #paradox example: @Paradox Who am i?
@@ -98,13 +107,25 @@ $allow = false;
 }
 
 $datas  = parse_ini_string(file_get_contents( 'config.ini' ), true);
+
 include 'addons/granted_channels.php'; // this is a list of channels the bot has admin rights to.
+$firstTime=$datas['settings']['firstTime'];
+$isOwner=$datas['settings']['owner'];
+
+ if($firstTime == "Yes")
+ {
+ if(($c < 1) && ($datas['settings']['owner'] != ""))
+ {
+ echo "Welcome! I see it's your first time \n You need to type " . $datas['settings']['Prefix'] . "grantchannel in your server for me to work!. \n  \n\n";
+ }
+ }
 
 if($c > 0)
 {
 include 'addons/afk_bot.php'; // Afk bot 
 include 'addons/channel_link_protection.php'; // Channel link protection
 include 'addons/isMuted.php';
+
 $ad=0;
 $am=0;
 
@@ -117,41 +138,11 @@ if((isset($dat[1])) && ($message->author->username != "Paradox")) // This is the
 {
 include 'addons/chkowner.php'; // checks to see if user is my master or not.
 
-if(($rawdat == "restart") && ($ad > 0)) { include 'commands/restart.php'; } // Restarts the bot.
-if($rawdat == "mimic") { include 'commands/mimic.php'; } // returns the text you've typed.
-if($rawdat == "afk") { include 'commands/afk.php'; } // adds an afk message, if someone mentions you it will reply for you.
-if($rawdat == "back") { include 'commands/back.php'; } // deletes your afk message.
-if($rawdat == "commands") { include 'commands/commands.php'; } // Shows a list of commands
-if($rawdat == "status") { include 'commands/status.php'; } // changes Paradox bot status
-if($rawdat == "getstatus") { include 'commands/getstatus.php'; } // gets user status
-if($rawdat == "getid") { include 'commands/getid.php'; } // gets user id
-if($rawdat == "getavatar") { include 'commands/getavatar.php'; } // gets user avatar.
-if(($rawdat == "purge") && ($ad > 0)) { include 'commands/purge.php'; } // deletes x amount of messages from channel.
-if($rawdat == "info") { include 'commands/info.php'; } // displays bot information
-if(($rawdat == "kick") && ($ad > 0)) { include 'commands/kick.php'; } // kicks a user from server.
-if(($rawdat == "addmaster") && ($ad > 0)) { include 'commands/addmaster.php'; } // adds a user to my masters list.
-if(($rawdat == "delmaster") && ($ad > 0)) { include 'commands/delmaster.php'; } // removes a user from my masters list.
-if($rawdat == "role") { include 'commands/role.php'; } // This command is still in the making.
-if(($rawdat == "mkchan") && ($ad > 0)) { include 'commands/mkchan.php'; } // Creates channel in server.
-if(($rawdat == "grantchannel") && ($ad > 0)) { include 'commands/grantchannel.php'; } // Allows the bot to moderate on your channel
-if(($rawdat == "stopchannel") && ($ad > 0)) { include 'commands/stopchannel.php'; } // Stops the bot from moderating on channel.
-if(($rawdat == "allowlinks") && ($ad > 0)) { include 'commands/allowlinks.php'; } // Allows links on server
-if(($rawdat == "denylinks") && ($ad > 0)) { include 'commands/denylinks.php'; } // Removes links on server unless the user is on my masters list.
-if(($rawdat == "log") && ($ad > 0)) { include 'commands/log.php'; } // posts a log into the bot_log channel.
-if(($rawdat == "logserver") && ($ad > 0)) { include 'commands/logserver.php'; } // sets a server for your bot log.
-if(($rawdat == "logchannel") && ($ad > 0)) { include 'commands/logchannel.php'; } // sets a channel for your bot log.
-if($rawdat == "reportuser") { include 'commands/reportuser.php'; } // reports a user.
-if($rawdat == "listreports") { include 'commands/listreports.php'; } // lists reported users.
-if($rawdat == "delreport") { include 'commands/delreport.php'; } // deletes a report on a user usage: #delreport <username>
-if($rawdat == "mute") { include 'commands/mute.php'; } // deletes user's text in chat. #mute <user>
-if($rawdat == "unmute") { include 'commands/unmute.php'; } // deletes a report on a user usage: #delreport <username>
-if($rawdat == "giphy") { include 'commands/giphy.php'; } // posts a random giphy img #giphy <keyword>
-if($rawdat == "sticker") { include 'commands/sticker.php'; } // posts a random giphy sticker #sticker <keyword>
-if($rawdat == "weather") { include 'commands/weather.php'; } // get's the weather from a zip code #weather <ZIPCODE> or <CITYNAME>
-if($rawdat == "shorturl") { include 'commands/shorturl.php'; } // uses bit.ly #shorturl <url> you have to setup a OAUTH key from bit.ly then place in config.ini under [apis].
+// ################# Bot commands ####################
+include 'master_cmds.php';
+// end #############
 
 } // isset end
-
 
 
 		$reply = $message->timestamp->format('F Y h:i:s A') . ' - '; // Format the message timestamp.
@@ -162,6 +153,9 @@ if($rawdat == "shorturl") { include 'commands/shorturl.php'; } // uses bit.ly #s
 		
 		
 	}	
+
+
+include 'addons/chkbot.php'; // checks if the user is new, if the config is setup properly. this addon will walk you through it.
 
 
 	});
