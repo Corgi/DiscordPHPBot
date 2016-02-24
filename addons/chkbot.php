8d5e743d6d5729037645682fa4ee890e389b3bc8
Joshua Cleland <proxikal@users.noreply.github.com>
2016-02-23 19:16:16 -0500

    Added update and important message system.

--- addons/chkbot.php
+++ addons/chkbot.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $ini = new INI('config.ini');
 $d = $ini->read('config.ini');
 $prefix=$d['settings']['Prefix'];
@@ -52,4 +53,81 @@ echo "###### You can add or remove servers in the future using: grantchannel and
 
 } // end of checking is owner is set in config file.
 
+
+
+// ###### Check if the bot needs an update. ###########
+
+if(time() >= $_SESSION['check_update'] + (60 * 60 * 2))
+{
+
+// version link: https://raw.githubusercontent.com/proxikal/DiscordPHPBot/master/version.txt
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+		'header'=>"User-Agent: proxikal"
+			  
+    ),
+); 
+
+$online=file_get_contents("https://raw.githubusercontent.com/proxikal/DiscordPHPBot/master/version.txt", false, stream_context_create($arrContextOptions));
+$online=str_replace("\n", "", $online);
+
+if (file_exists("version.txt")) {
+$mine=file_get_contents("version.txt");
+}
+else
+{
+echo "### Missing File: version.txt \n make sure this file is in the Paradox\DiscordPHPBot folder! \n";
+}
+
+if($online > $mine)
+{
+echo "### New Version: " . $online . " Is Available on Github! \n";
+}
+else
+{
+echo "## Checked for updates. No new version available. \n";
+}
+$_SESSION['check_update'] = time();
+} // end of time check
+
+
+// #### Check and see if there's a message from the developers!
+// url https://raw.githubusercontent.com/proxikal/DiscordPHPBot/master/msg.txt
+if(time() >= $_SESSION['check_message'] + (60 * 20))
+{
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+		'header'=>"User-Agent: proxikal"
+			  
+    ),
+); 
+
+$online=file_get_contents("https://raw.githubusercontent.com/proxikal/DiscordPHPBot/master/msg.txt", false, stream_context_create($arrContextOptions));
+$on=explode("=>", $online);
+$od=explode("break;", $on[1]);
+
+$themsg = " ";
+foreach($od as $item)
+{
+$themsg .= $item . " \n";
+
+}
+
+
+if($on[0] != $d['msgcode']['code'])
+{
+$ini->data['msgcode']['code'] = $on[0];
+$ini->write('config.ini');
+$guild = $discord->guilds->get('name', $d['log']['server']);
+$channel = $guild->channels->get('name', $d['log']['channel']); // you can change 'name' to any other attribute if you want
+$channel->sendMessage(":speech_balloon:" . $themsg);
+
+}
+
+$_SESSION['check_message'] = time();
+} // end of time check
 ?>
