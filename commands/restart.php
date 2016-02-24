3b97e450040ecf27c7844c77d2f223414fba2661
Joshua Cleland <proxikal@users.noreply.github.com>
2016-02-24 17:55:59 -0500

    wasn't working, now it's fixed for everyone.

--- commands/restart.php
+++ commands/restart.php
@@ -2,15 +2,24 @@
 $ini = new INI('config.ini');
 $d = $ini->read('config.ini');
 
-if($d['commands']['restart'] == "1")
+	if($d['commands']['restart'] == "1")
+	{
+if($message->author->username == $d['settings']['owner'])
 {
 
-	$message->channel->sendMessage(":heavy_exclamation_mark: Restarting Bot");
-	exec('c:\WINDOWS\system32\cmd.exe /c START '. $_SERVER['DOCUMENT_ROOT'] . '\Paradox\DiscordPHPBot\Restart.bat');
-	die;
+$message->channel->sendMessage(":heavy_exclamation_mark: Restarting Bot");
+exec('c:\WINDOWS\system32\cmd.exe /c START '. $_SERVER['DOCUMENT_ROOT'] . '\Paradox\DiscordPHPBot\Restart.bat');
+die;
+}
+else
+{
+$message->channel->sendMessage(":exclamation: Only the Bot Owner: **".$d['settings']['owner']."** can Restart me.");
+} // end of bot owner check
+
 	}
 	else
 	{
 	$message->reply("This command is disabled.");
 	}
+
 ?>
