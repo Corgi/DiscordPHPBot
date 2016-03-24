08378b1263a57a765467419bbe855e3d61b0f3ad
Joshua Cleland <proxikal@users.noreply.github.com>
2016-03-24 18:05:53 -0400

    Fixed error

--- commands/giphy.php
+++ commands/giphy.php
@@ -16,7 +16,7 @@ $msg=str_replace($pref."giphy ", "", $message->content);
 $msg=str_replace(" ", "+", $msg);
 
  $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' . $msg);
+    curl_setopt($ch, CURLOPT_URL, 'http://api.giphy.com/v1/gifs/random?api_key=YOURAPIKEYHERE&tag=' . $msg);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $data = curl_exec($ch);
 	$json = json_decode($data);
