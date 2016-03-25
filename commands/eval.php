6663697a8c0944eaa36b9dcd7110002449c30f9f
Joshua Cleland <proxikal@users.noreply.github.com>
2016-03-25 19:54:08 -0400

    Ty david ^.^

--- commands/eval.php
+++ commands/eval.php
@@ -1,8 +1,4 @@
 <?php
-$ini = new INI('config.ini');
-$d = $ini->read('config.ini');
-$iniP = new INI('inis/Prefix.ini');
-$p = $iniP->read('inis/Prefix.ini');
 $pref=$p[$message->full_channel->guild->id]['Prefix'];
 $msg=str_replace($pref."eval ", "", $message->content);
 if($pref == "")
@@ -11,32 +7,50 @@ $pref=$thepref;
 }
 
 
-if($d['settings']['owner'] == $message->author->username)
+if($d['settings']['owner'] == $message->author->id)
 {
-	if (!isset($msg)) {
- // empty params
-		}
+
+
+
+
 		set_error_handler(function ($errno, $errstr) {
 			if (!(error_reporting() & $errno)) {
-				// it worked!
+				return;
 			}
 			echo "[Eval Error] {$errno} {$errstr}\r\n";
 			throw new \Exception($errstr, $errno);
 		}, E_ALL);
 		
 		try {
-			eval('$response = '.implode(' ', $params).';');
+			$params = implode(' ', $msg);
+			$params = str_replace('```', '', $params);
+			$params = "<?php\r\n".$params;
+			dump($params);
+			$fileName = BOT_DIR.'/eval/'.Str::random();
+			file_put_contents($fileName, $params);
+			// lint
+			$lint = shell_exec('php -l '.$fileName);
+			if (strpos($lint, 'Errors parsing') !== false) {
+				$global->sendMessage("Erorrs linting the file: ```{$lint}```");
+				restore_error_handler();
+				return;
+			}
+			$response = require_once $fileName;
 			if (is_string($response)) {
 				$response = str_replace(DISCORD_TOKEN, 'TOKEN-HIDDEN', $response);
-				$response = str_replace($config['password'], 'PASSWORD-HIDDEN', $response);
-				$response = str_replace($config['sudo_pass'], 'SUDO-HIDDEN', $response);
+				$response = str_replace($config['token'], 'TOKEN-HIDDEN', $response);
 			}
-			$global->sendMessage("`{$response}`");
+			$global->sendMessage("```\r\n{$response}\r\n```");
+
 		} catch (\Exception $e) {
 			$global->sendMessage("Eval failed. {$e->getMessage()}");
 		}
 		restore_error_handler();
-}
+
+
+
+
+} // make sure it's my owner.
 
 
 		?>
