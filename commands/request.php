cdbab83a86a19cec5893c84e0cdf9d548d649339
EdibleDerpy <EdibleDerpy@gmail.com>
2016-03-27 17:40:26 -0600

    Update request.php

--- commands/request.php
+++ commands/request.php
@@ -1,10 +1,4 @@
 <?php
-use Discord\Exceptions\PartRequestFailedException;
-
-$ini = new INI('config.ini');
-$d = $ini->read('config.ini');
-$iniP = new INI('inis/Prefix.ini');
-$p = $iniP->read('inis/Prefix.ini');
 $pref=$p[$message->full_channel->guild->id]['Prefix'];
 if($pref == "")
 {
