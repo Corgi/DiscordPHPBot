5dcb43828f6cbfd03947a4db68c425bf061d9f9a
Joshua Cleland <proxikal@users.noreply.github.com>
2016-03-27 10:29:57 -0400

    Added autorole.

--- events/guild_member_add.php
+++ events/guild_member_add.php
@@ -6,6 +6,54 @@ $isguild=$data->guild_id;
  // $masters  = parse_ini_string(file_get_contents( 'inis/masters.ini' ), true);
 $iniD = new INI('inis/masters.ini');
 $dD = $iniD->read('inis/masters.ini');
+
+
+if($dD[$data->guild_id]['Auto'] != "")
+{
+$therole=$dD[$data->guild_id]['Auto'];
+    $guild = $discord->guilds->get('id', $data->guild_id);
+    // And now the user to change.
+   // $member = $guild->members->first();
+    if(isset($guild))
+    {
+//      echo "Found the Guild!".PHP_EOL;
+
+// echo "Found the member!".PHP_EOL;
+    // And the role to remove.
+   // $role = $member->roles->first();
+        try
+        {
+        $role = $guild->roles->get('name', $therole);
+        } catch (PartRequestFailedException $e) {
+            $message->channel->sendMessage("I don't have permissions to manage roles in this server");
+        }
+    $member = $guild->members->get('id', $data->user->id);
+
+    if((isset($role)) && (isset($member)))
+    {
+    //  echo "Saving the role!".PHP_EOL;
+     $member->addRole($role);
+    $member->save(); // Remember: You MUST save after changing roles.
+
+$greetchannel=$dD[$newguild]['GreetChannel'];
+$channel = $guild->channels->get('name', $greetchannel);
+if(!isset($channel))
+{
+$channel = $guild->channels->getAll('type', 'text')->first();
+}
+    $channel->sendMessage("`Auto Role` => <@".$data->user->id."> To => `".$therole."`");
+
+    }
+    else
+    {
+        $message->channel->sendMessage("`Auto Role` => Failed to find the role.");
+    } // make sure the role exists.
+
+    } // check to make sure the guild exists.
+}
+
+
+
 if($dD[$isguild]['GreetMsg'] != "")
 {
     $guild = $discord->guilds->get('id', $data->guild_id);
@@ -17,7 +65,6 @@ if($dD[$isguild]['GreetMsg'] != "")
     $new_member = $data->user;
     $newguild=$data->guild_id;
     $guild_name=$guild->name;
-
     $res="The Message didn't work";
     $r=0;
 // echo "NEW USER: " . $new_member->username . " GUILD: " . $guild->name.PHP_EOL;
@@ -32,7 +79,7 @@ $channel = $guild->channels->getAll('type', 'text')->first();
 }
 
 $greetmsg = $dD[$newguild]['GreetMsg'];
-$greetmsg=str_replace("{user}", $new_member->username, $greetmsg);
+$greetmsg=str_replace("{user}", "<@".$new_member->id.">", $greetmsg);
 $greetmsg=str_replace("{guild}", $guild->name, $greetmsg);
 
 try
