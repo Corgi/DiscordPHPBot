c63e5db478c782b544fe30f94791e232d8243e66
Joshua Cleland <proxikal@users.noreply.github.com>
2016-03-27 18:40:24 -0400

    fixed set.php

--- commands/set.php
+++ commands/set.php
@@ -5,8 +5,6 @@ $ini = new INI('config.ini');
 $d = $ini->read('config.ini');
 $iniP = new INI('inis/Prefix.ini');
 $p = $iniP->read('inis/Prefix.ini');
-$iniB = new INI('inis/badwords.ini');
-$dB = $iniB->read('inis/badwords.ini');
 $iniM = new INI('inis/masters.ini');
 $dM = $iniM->read('inis/masters.ini');
 $pref=$p[$message->full_channel->guild->id]['Prefix'];
@@ -50,39 +48,6 @@ if(strtolower($opt[0]) == "greetmsg")
 
 // #######################################################################
 
-
-
-
-
-if(isset($opt[1]))
-{
-
-
-if(strtolower($opt[0]) == "filtermsg")
-{
-			$message->channel->sendMessage("My `Filter Message` has been changed to: \n ```".$word."```");
-			$iniM->data[$message->full_channel->guild->id]['FilterMsg'] = $word;
-			$iniM->write('inis/masters.ini');
-
-} // end of add greetmsg
-
-} // end of isset
-
-
-
-if(isset($opt[1]))
-{
-
-
-if(strtolower($opt[0]) == "linkmsg")
-{
-			$message->channel->sendMessage("My `Ant-Link Message` has been changed to: \n ```".$word."```");
-			$iniM->data[$message->full_channel->guild->id]['LinkMsg'] = $word;
-			$iniM->write('inis/masters.ini');
-
-} // end of add greetmsg
-
-} // end of isset
 
 } // end of owner check
 } // end of pm check, this command only works in server
